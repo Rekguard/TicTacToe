83e254c0513bbd73381ec151bdd31c4c729d395e
Jonathan O'Neill <ignieire@gmail.com>
2014-03-03 12:49:07 +0000

    input() method added to MouseControl Class
    Mouse clicks now output the co-ordinates for X and Y position

--- src/Controls/MouseControl.java
+++ src/Controls/MouseControl.java
@@ -1,5 +1,22 @@
 package Controls;
 
+import org.lwjgl.input.Mouse;
+
 public class MouseControl {
+	
+	public void input(int screenHeight){
+		int mouseX = Mouse.getX();
+			/* OpenGL Y-Axis is always inverted! */
+		int mouseY = (screenHeight-1)- Mouse.getY();
+		
+		try{
+			if (Mouse.isButtonDown(0)) {
+				System.out.println("Position: X = " + mouseX + " Y = "+ mouseY);
+			}
+			Thread.sleep(100);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
 
 }
